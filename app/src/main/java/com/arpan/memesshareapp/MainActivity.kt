cf0e4c8a4694061deb1e44ea374080588c958240
Arpan Das <dasarpan471@gmail.com>
2022-09-04 12:14:24 +0530

    done

---
+++ app/src/main/java/com/arpan/memesshareapp/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.arpan.memesshareapp
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var memeImageView: ImageView
+    private lateinit var progressBar: ProgressBar
+    var currentImageUrl: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        memeImageView = findViewById(R.id.memeImageView)
+        progressBar = findViewById(R.id.progressBar)
+        loadMeme()
+    }
+
+private fun loadMeme(){
+
+// ...
+    progressBar.visibility = View.VISIBLE
+// Instantiate the RequestQueue.
+
+    val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+    val jsonRequest = JsonObjectRequest(
+        Request.Method.GET, url,null,
+        Response.Listener{ response ->
+            currentImageUrl= response.getString("url")
+            Glide.with(this).load(currentImageUrl).listener(object: RequestListener<Drawable>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBar.visibility = View.GONE
+                    return false
+                }
+
+            }).into(memeImageView)
+        },
+        Response.ErrorListener {})
+
+// Add the request to the RequestQueue.
+    MySingleTon.getInstance(this).addToRequestQueue(jsonRequest)
+}
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Check this cool meme I found \n $currentImageUrl")
+        val chooser = Intent.createChooser(intent, "Share this meme using...")
+        startActivity(chooser)
+    }
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+}
---
+++ app/src/main/java/com/arpan/memesshareapp/MySingleTon.kt
@@ -0,0 +1,29 @@
+package com.arpan.memesshareapp
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.Volley
+
+class MySingleTon constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: MySingleTon? = null
+        fun getInstance(context: Context) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: MySingleTon(context).also {
+                    INSTANCE = it
+                }
+            }
+    }
+
+    val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
